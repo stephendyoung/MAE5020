8f214f00c747f24011fdc04012c1c05af3766180
Stephen Young <stephendyoung02@gmail.com>
2025-02-03 15:02:07 -0600

    made new directory and have update for hw files

--- HW1/SVDSchileren.py
+++ HW1/SVDSchileren.py
@@ -34,24 +34,39 @@ print("Shape of VT    :", VT.shape)
 S = np.diag(S)
 print("np.diag(S)     :", S.shape)
 
-j = 0
 
-for r in (10, 20, 40):
-    Xapprox =  U[:,:r] @ S[0:r,:r] @ VT[:r,:]
-    print("%%%%%%%%%%%%%%%%%%%%%")
-    print("--------------r:",r)
-    print("Shape of U     :", U[:,:r].shape)
-    print("Shape of S     :", S[0:r,:r].shape)
-    print("Shape of VT    :", VT[:r,:].shape)
-    print("Shape of Xaprx :", Xapprox.shape)
-    
-    plt.figure(j+1)
-    j +=1
-    img = plt.imshow(Xapprox)
-    img.set_cmap('gray')
-    plt.axis('off')
-    plt.title('r = ' + str(r))
-    plt.show()   
-    
 
- 
+S_full = np.linalg.svd(X, compute_uv=False)  
+InfRetained = np.cumsum(S_full**2) / np.sum(S_full**2)
+print(InfRetained)
+k_90 = np.searchsorted(InfRetained, 0.90) + 1
+print(f"Rank needed to retain 90% of information: {k_90}")
+
+
+# j = 0
+# for r in (10, 20, 40):
+#     Xapprox =  U[:,:r] @ S[0:r,:r] @ VT[:r,:]
+#     print("%%%%%%%%%%%%%%%%%%%%%")
+#     print("--------------r:",r)
+#     print("Shape of U     :", U[:,:r].shape)
+#     print("Shape of S     :", S[0:r,:r].shape)
+#     print("Shape of VT    :", VT[:r,:].shape)
+#     print("Shape of Xaprx :", Xapprox.shape)
+    
+    
+        
+    
+#     plt.figure(j+1)
+#     j +=1
+#     img = plt.imshow(Xapprox)
+#     img.set_cmap('gray')
+#     plt.axis('off')
+#     plt.title('r = ' + str(r))
+#     plt.show()       
+
+r = 17
+Ur = U[:, :r]
+UTransposeCheck = Ur @ np.transpose(Ur)
+print(f"U*U^T is rxr check: {Ur @ Ur.T}")
+
+
